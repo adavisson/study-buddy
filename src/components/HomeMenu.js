7f418684b150897213dd44b326169df31fe51835
Andrew Davisson <akdavisson4@gmail.com>
2020-02-03 11:44:53 -0700

    updated home menu

--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Dropdown } from 'semantic-ui-react';
 
 const HomeMenu = () => {
   const [ activeItem, setActiveItem ] = useState("");
@@ -11,23 +11,33 @@ const HomeMenu = () => {
   return (
     <Menu>
       <Menu.Item
-        name='home'
-        active={activeItem === 'home'}
-        onClick={handleItemClick}
-        href="/"
-      >Home</Menu.Item>
-      <Menu.Item
-        name='javascript'
-        active={activeItem === 'javascript'}
-        onClick={handleItemClick}
-        href="/javascript"
-      >JavaScript</Menu.Item>
-      <Menu.Item
-        name='ruby'
-        active={activeItem === 'ruby'}
-        onClick={handleItemClick}
-        href="/ruby"
-      >Ruby</Menu.Item>
+            name='home'
+            active={activeItem === 'home'}
+            onClick={handleItemClick}
+            href="/"
+          >Home</Menu.Item>
+      <Dropdown item text='Categories'>
+        <Dropdown.Menu>
+          <Dropdown.Item
+            name='javascript'
+            active={activeItem === 'javascript'}
+            onClick={handleItemClick}
+            href="/javascript"
+          >JavaScript</Dropdown.Item>
+          <Dropdown.Item
+            name='ruby'
+            active={activeItem === 'ruby'}
+            onClick={handleItemClick}
+            href="/ruby"
+          >Ruby</Dropdown.Item>
+          <Dropdown.Item
+            name="html-css"
+            active={activeItem === 'html-css'}
+            onClick={handleItemClick}
+            href="/html-css"
+          >HTML/CSS</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </Menu>
   );
 }
