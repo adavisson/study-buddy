f70842645dea16673e30e743d39136d0f95ff1f1
Andrew Davisson <akdavisson4@gmail.com>
2020-02-03 13:37:34 -0700

    new build

---
+++ build/precache-manifest.8e56ad3b6632c5fd49473ed3aaf5968c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955583cbc1891edd6b348ab815f510f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43dffbdb8e4d45f43246",
+    "url": "/static/css/main.bc027931.chunk.css"
+  },
+  {
+    "revision": "29fb8d82e7c1a949adcb",
+    "url": "/static/js/2.fab3f917.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/static/js/2.fab3f917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dffbdb8e4d45f43246",
+    "url": "/static/js/main.c6d00f30.chunk.js"
+  },
+  {
+    "revision": "52bf078c6daa1dc3d6d3",
+    "url": "/static/js/runtime-main.f477ea98.js"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/static/media/JavaScript-logo.0cded3a3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
