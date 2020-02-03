ba38b5f4465beec626f2cc49dcf7cd4b297e4a9a
Andrew Davisson <akdavisson4@gmail.com>
2020-02-03 10:05:29 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import { Header } from 'semantic-ui-react';
+
 
 function App() {
   return (
