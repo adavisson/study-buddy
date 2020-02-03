0e43093aff0074769359256e0c91855f609c56af
Andrew Davisson <akdavisson4@gmail.com>
2020-02-03 10:02:03 -0700

    added javascriptcard component

---
+++ src/components/JavaScriptCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+const JavaScriptCard = () => {
+  return (
+    <Card>
+      <Image src='' wrapped ui={false}/>
+    </Card>
+  );
+}
+ 
+export default JavaScriptCard;
