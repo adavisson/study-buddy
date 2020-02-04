cc068c2ef71ab003ab122530440dbd0aa6c98eea
Andrew Davisson <akdavisson4@gmail.com>
2020-02-03 21:00:36 -0700

    added link to website

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div>
       <Header as="h1">About</Header>
-      <p>This site serves as a study tool for common coding interview questions.</p>
+      <p>This site serves as a study tool for common coding interview questions. View my <a href="https://andydavisson.com">website</a> to learn more about me.</p>
     </div>
   );
 }
