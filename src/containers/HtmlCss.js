43379f4c6533e094852295251595f9e5423d0b57
Andrew Davisson <akdavisson4@gmail.com>
2020-02-04 08:46:59 -0700

    added html and css questions

--- src/containers/HtmlCss.js
+++ src/containers/HtmlCss.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import FlashCard from './FlashCard';
 
 const HtmlCss = () => {
+  const questions = [
+    {
+      question: 'What are the 3 ways to integrate CSS?',
+      answer: 'Inline -> written as a part of the HTML element. External -> separate CSS file that is linked in the HTML. Internal -> written in a style tag inside the head element.'
+    },
+    {
+      question: 'Describe the difference between margin and padding...',
+      answer: 'Margin is the transparent space outside the border. Padding is the transparent space inside the border surrounding the content.'
+    },
+    {
+      question: 'What is the z-index?',
+      answer: 'The z-index helps specify the stack order of positioned elements that may overlap one another. Default value is zero and it can take either a positive or negative number.'
+    },
+    {
+      question: 'What are media queries?',
+      answer: 'Media queries are rules that apply different styles for different media types/devices. Media queries can be used to check things, like: width/height of viewport, width/height of device, orientation, and resolution.'
+    },
+    {
+      question: 'Describe the 3 levels of CSS specificity...',
+      answer: 'From the top: 1) Type selectors and pseudo-elements.  2) Class selectors, attribute selectors, and pseudo classes.  3) ID selectors.'
+    },
+    {
+      question: 'What does HTML stand for?',
+      answer: 'Hyper Text Markup Language'
+    },
+    {
+      question: 'What does CSS stand for?',
+      answer: 'Cascading Style Sheet'
+    },
+    {
+      question: 'What is semantic HTML?',
+      answer: 'A style that is more descriptive. Ex. Using <strong /> and <em /> instead of <b /> and <i />'
+    },
+    {
+      question: 'What is the iframe tag used for?',
+      answer: 'An iframe is used to display a web page within a web page.'
+    }
+  ];
+
   return (
     <div>
-      <Header as="h1">HTML/CSS</Header>
-      <p>These questions are coming soon.</p>
+      <FlashCard subject={"HTML/CSS"} questions={questions} />
     </div>
   );
 }
