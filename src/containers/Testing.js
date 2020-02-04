6bae3da4a340ada3ee9d96ae54b1031177bbea08
Andrew Davisson <akdavisson4@gmail.com>
2020-02-04 08:19:32 -0700

    Added more testing questions

--- src/containers/Testing.js
+++ src/containers/Testing.js
@@ -14,6 +14,42 @@ const Testing = () => {
     {
       question: 'What is System Testing?',
       answer: 'Testing that validates the complete and fully integrated software product.'
+    },
+    {
+      question: 'Describe TDD...',
+      answer: 'Test-Driven Develpment is where you write your tests before writing your code. Helps your functionality conform to requirements and discourages opinionated code/tests.'
+    },
+    {
+      question: 'When using Mocha, what does "describe" do?',
+      answer: '"describe" defines a group of tests.'
+    },
+    {
+      question: 'When using Mocha, what does "it" do?',
+      answer: '"it" defines a single test.'
+    },
+    {
+      question: 'When using Mocha, what does "before" do?',
+      answer: '"before" goes inside "describe" and runs before ANY tests in that group.'
+    },
+    {
+      question: 'When using Mocha, what does "beforeEach" do?',
+      answer: '"beforeEach" goes inside "describe" and runs before EVERY test in that group. Can be very expensive.'
+    },
+    {
+      question: 'When using Mocha, what does "after" do?',
+      answer: '"after" goes inside "describe" and runs after ANY tests in that group.'
+    },
+    {
+      question: 'When using Mocha, what does "afterEach" do?',
+      answer: '"afterEach" goes insded "describe" and runs after EVERY test in that group. Can be very expensive'
+    },
+    {
+      question: 'What is Chai?',
+      answer: 'Chai is an assertion library. An assertion evaluates that a condition is true.'
+    },
+    {
+      question: 'What is Mocha?',
+      answer: 'Mocha is a test runner.'
     }
   ];
 
