a9082eca699f77bb0afb06935434f2f58293339d
Andrew Davisson <akdavisson4@gmail.com>
2020-02-28 15:36:08 -0700

    Added more Java questions

--- src/containers/Java.js
+++ src/containers/Java.js
@@ -14,6 +14,22 @@ const Java = () => {
     {
       question: 'What is the "this" keyword in Java?',
       answer: 'Within an instance method or constructor, "this" is a reference to the current object.'
+    },
+    {
+      question: 'What does "JVM" stand for and what is it?',
+      answer: 'A Java Virtual Machine is responsible for converting byte code into machine readable code.'
+    },
+    {
+      question: 'Which class is the superclass of all other classes?',
+      answer: 'java.lang.Object'
+    },
+    {
+      question: 'What is method overloading?',
+      answer: 'When you have more than one method with the same name in a single class, but the arguments are different.'
+    },
+    {
+      question: 'What is the "final" keyword?',
+      answer: 'The "final" keyword is used in classes to make sure no other class can extend it. It is used for methods to make sure that child classes can not override it. It is used with variables to make sure that they can only be assigned once.'
     }
   ]
 
