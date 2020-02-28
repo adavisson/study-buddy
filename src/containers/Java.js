d1ee7bdd912c82bf4aca6717405c491ddabc5114
Andrew Davisson <akdavisson4@gmail.com>
2020-02-27 19:29:52 -0700

    Added a few java questions

--- src/containers/Java.js
+++ src/containers/Java.js
@@ -4,8 +4,16 @@ import FlashCard from './FlashCard';
 const Java = () => {
   const questions = [
     {
-      question: "Question",
-      answer: "Answer"
+      question: 'List the three steps for creating an Object for a class.',
+      answer: 'An Object is first declared, then instantiated, and then initialize'
+    },
+    {
+      question: 'What is a class variable?',
+      answer: 'A variable declared in a class with the static keyword, but outside any method.'
+    },
+    {
+      question: 'What is the "this" keyword in Java?',
+      answer: 'Within an instance method or constructor, "this" is a reference to the current object.'
     }
   ]
 
